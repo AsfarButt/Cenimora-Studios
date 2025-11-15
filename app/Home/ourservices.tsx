1150c94c73ed8bb3962f4ab3a0a1d15e098eddd0
AsfarButt <asfarbutt69@gmail.com>
2025-11-15 21:18:40 +0500

    Update 4

--- app/Home/ourservices.tsx
+++ app/Home/ourservices.tsx
@@ -8,10 +8,21 @@ const cormorant = Cormorant_Garamond({
 
 export default function OutServices(){
 
-    return(<div className="relative w-full pb-100 h-auto flex flex-col justify-center items-center bg-black/98">
+    return(<div className="relative w-full pt-30 pb-100 h-auto flex flex-col justify-center items-center bg-black/98">
         <div className="relative w-full max-w-220 mx-2 md:mx:6 flex flex-col justify-center items-center">
-            <h1 className="text-white/90 text-4xl lg:text-5xl">Selected <span className={`${cormorant.className} text-[40px] lg:text-[52px] text-blue-700/90`}>Work</span></h1>
+            <h1 className="text-white/90 text-4xl lg:text-5xl"><span className={`${cormorant.className} text-[40px] lg:text-[52px] text-blue-700/90`}><span className="text-[43px] lg:text-[56px]">C</span>lient </span>Reviews</h1>
             <p className="text-white/65 mt-4 text-center text-[12px] px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro assumenda dicta, nihil magnam eaque cumque vitae eius tempora sunt maxime minima eos aut rerum eligendi ipsam e adipisci autem, necessitatibus nemo explicabo, quos eaque voluptatibus, minima odit cum corrupti quasi rerum repellat quis optio eligendi.</p>
+        </div>
+
+        <div className="relative w-full h-full bg-white/20 flex justify-center items-center">   
+            <div className="relative top-32 w-20 h-20 flex justify-center items-center bg-white/20 rounded-full shadow-lg shadow-blue-500/30">
+                <div className="relative w-12 mr-1.5 mt-1.5 h-10 scale-80 ">
+                    <div className="absolute left-1 top-1 h-[80%] w-[80%] bg-white rounded-2xl z-2"></div>
+                    <div className="absolute -top-px -right-px w-[95%] h-[90%] rounded-[13px] bg-black z-1" />
+                    <div className="relative w-12 h-12 mask-[url('/message3.png')] bg-blue-600 mask-cover z-3" />
+                    <div className="absolute -top-2 -right-1.5 w-full h-[90%] bg-white/85 rounded-xl" />
+                </div>
+            </div>
         </div>
 
     </div>)
--- app/Home/selectedwork.tsx
+++ app/Home/selectedwork.tsx
@@ -8,7 +8,7 @@ const cormorant = Cormorant_Garamond({
 
 export default function SelectedWork(){
 
-    return(<div className="relative w-full pb-100 h-auto flex flex-col justify-center items-center bg-black/98">
+    return(<div className="relative w-full h-auto flex flex-col justify-center items-center bg-black/98">
         <div className="relative w-full max-w-220 mx-2 md:mx:6 flex flex-col justify-center items-center">
             <h1 className="text-white/90 text-4xl lg:text-5xl">Selected <span className={`${cormorant.className} text-[40px] lg:text-[52px] text-blue-700/90`}>Work</span></h1>
             <p className="text-white/65 mt-4 text-center text-[12px] px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro assumenda dicta, nihil magnam eaque cumque vitae eius tempora sunt maxime minima eos aut rerum eligendi ipsam e adipisci autem, necessitatibus nemo explicabo, quos eaque voluptatibus, minima odit cum corrupti quasi rerum repellat quis optio eligendi.</p>
@@ -18,7 +18,7 @@ export default function SelectedWork(){
 
                 <div className="relative w-auto h-auto flex items-center justify-center flex-col md:flex-row">
                     <div className="relative w-100 h-75 md:w-75 md:h-80 transition-all duration-400 m-4 md:mx-6 lg:mx-8 rounded-2xl bg-[url('/bgimage1.jpg')] bg-center bg-cover shadow-lg shadow-blue-200/40 before:absolute before:inset-0 before:bg-black/30 before:z-1 overflow-hidden group">
-                        <div className="absolute bottom-0 overflow-hidden h-[28%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 w-[96%] m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
+                        <div className="absolute bottom-0 overflow-hidden h-[28%] w-[98%] lg:w-[97%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
                         <div className="py-1"><div className="relative m-1 w-9 h-9 rounded-full [box-shadow:0px_0px_3px_rgba(270,270,270,0.4)] text-white text-2xl font-bold font-sans flex items-center justify-center before:transition-all before:-z-1 before:duration-400 before:absolute before:inset-0 before:bg-blue-600/60 overflow-hidden before:opacity-0 group-hover:before:opacity-100">1</div></div>
                         <div className=""><h1 className="text-[20px] pl-2 text-medium text-white">Graphics Designing</h1>
                         <p className="text-[14px] text-white/85 px-2 opacity-0 group-hover:opacity-100">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore eaque libero soluta hic numquam! Tempor</p>
@@ -28,7 +28,7 @@ export default function SelectedWork(){
                     </div>
 
                     <div className="relative w-100 h-75 md:w-75 md:h-80 transition-all duration-400 m-4 md:mx-6 lg:mx-8 rounded-2xl bg-[url('/bgimage2.jpg')] bg-center bg-cover shadow-lg shadow-blue-200/40 before:absolute before:inset-0 before:bg-black/30 before:z-1 overflow-hidden group">
-                        <div className="absolute bottom-0 overflow-hidden h-[28%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 w-[96%] m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
+                        <div className="absolute bottom-0 overflow-hidden h-[28%] w-[98%] lg:w-[97%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
                         <div className="py-1"><div className="relative m-1 w-9 h-9 rounded-full [box-shadow:0px_0px_3px_rgba(270,270,270,0.4)] text-white text-2xl font-bold font-sans flex items-center justify-center before:transition-all before:-z-1 before:duration-400 before:absolute before:inset-0 before:bg-blue-600/60 overflow-hidden before:opacity-0 group-hover:before:opacity-100">2</div></div>
                         <div className=""><h1 className="text-[20px] pl-2 text-medium text-white">Web Development</h1>
                         <p className="text-[14px] text-white/85 px-2 opacity-0 group-hover:opacity-100">Lorem, ipsum dolor sit amet consectetur  elit. Labore eaque libero soluta hic ! Tempor</p>
@@ -40,7 +40,7 @@ export default function SelectedWork(){
 
                 <div className="relative w-auto h-auto flex items-center justify-center flex-col md:flex-row">
                     <div className="relative w-100 h-75 md:w-75 md:h-80 transition-all duration-400 m-4 md:mx-6 lg:mx-8 rounded-2xl bg-[url('/bgimage3.jpg')] bg-center bg-cover shadow-lg shadow-blue-200/40 before:absolute before:inset-0 before:bg-black/30 before:z-1 overflow-hidden group">
-                        <div className="absolute bottom-0 overflow-hidden h-[28%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 w-[96%] m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
+                        <div className="absolute bottom-0 overflow-hidden h-[28%] w-[98%] lg:w-[97%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
                         <div className="py-1"><div className="relative m-1 w-9 h-9 rounded-full [box-shadow:0px_0px_3px_rgba(270,270,270,0.4)] text-white text-2xl font-bold font-sans flex items-center justify-center before:transition-all before:-z-1 before:duration-400 before:absolute before:inset-0 before:bg-blue-600/60 overflow-hidden before:opacity-0 group-hover:before:opacity-100">3</div></div>
                         <div className=""><h1 className="text-[20px] pl-2 text-medium text-white">AI & Automation</h1>
                         <p className="text-[14px] text-white/85 px-2 opacity-0 group-hover:opacity-100">Polor sit amet consectetur adipisicing elit. Labore eaque  soluta hic numquam! Tempor</p>
@@ -50,7 +50,7 @@ export default function SelectedWork(){
                     </div>
 
                     <div className="relative w-100 h-75 md:w-75 md:h-80 transition-all duration-400 m-4 md:mx-6 lg:mx-8 rounded-2xl bg-[url('/bgimage4.jpg')] bg-center bg-cover shadow-lg shadow-blue-200/40 before:absolute before:inset-0 before:bg-black/30 before:z-1 overflow-hidden group">
-                        <div className="absolute bottom-0 overflow-hidden h-[28%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 w-[96%] m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
+                        <div className="absolute bottom-0 overflow-hidden h-[28%] w-[98%] lg:w-[97%] [box-shadow:0px_0px_5px_rgba(0,0,0,0.4)] before:absolute before:inset-0 before:bg-black/1 group-hover:before:bg-black/10 before:transition-all before:duration-400 m-1 transition-all duration-400 ease-in backdrop-blur-sm rounded-2xl group-hover:w-full group-hover:h-full group-hover:m-0">
                         <div className="py-1"><div className="relative m-1 w-9 h-9 rounded-full [box-shadow:0px_0px_3px_rgba(270,270,270,0.4)] text-white text-2xl font-bold font-sans flex items-center justify-center before:transition-all before:-z-1 before:duration-400 before:absolute before:inset-0 before:bg-blue-600/60 overflow-hidden before:opacity-0 group-hover:before:opacity-100">4</div></div>
                         <div className=""><h1 className="text-[20px] pl-2 text-medium text-white">Ad Development</h1>
                         <p className="text-[14px] text-white/85 px-2 opacity-0 group-hover:opacity-100">Lorem, ipsum dolor sit amet  adipisicing elit. Labore  libero soluta hic numquam! s</p>
