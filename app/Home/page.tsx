b1fd6a0a4f19fe01a14c85855acf14c5b97ea736
AsfarButt <asfarbutt69@gmail.com>
2025-11-17 00:59:03 +0500

    Update 7

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -3,17 +3,20 @@ import HeroSection from "./herosection";
 import SelectedWork from "./selectedwork";
 import ClientReviews from "./clientreviews";
 import Testimonials from "./testimonials";
-
+import Ratings from "./ratings";
 
 
 export default function Page(){
 
 
     return(<div className="relative w-screen mb-500 h-auto">
+        
         <Header />
         <HeroSection />
         <SelectedWork />
         <ClientReviews />
         <Testimonials />
+        <Ratings />
+
     </div>)
 }
---
+++ app/Home/ratings.tsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+export default function Ratings(){
+
+    return(<div className="relative w-full py-20 h-auto bg-black/98 flex justify-center items-center">
+        <div className="relative w-[90%] px-4 max-w-7xl h-auto flex flex-col gap-2 lg:gap-6 lg:flex-row items-center justify-evenly">
+            <div className="resize relative w-full h-38 px-2 my-auto rounded-2xl border border-white/25">
+            <h1 className="text-center pt-6 text-5xl text-blue-900/85 font-bold">165+</h1>
+            <p className="text-center text-white/65 text-26px pb-6 pt-3">Retention Systems Deployed Across 12+ Industries</p>
+            </div>
+            <div className="resize relative w-full h-38 px-2 my-auto rounded-2xl border border-white/25">
+            <h1 className="text-center pt-6 text-5xl text-blue-900/85 font-bold">$1 Million+</h1>
+            <p className="text-center text-white/65 text-26px pb-6 pt-3">Revenue Generated For Our Clients</p>
+            </div>
+            <div className="resize relative w-full h-38 px-2 my-auto rounded-2xl border border-white/25">
+            <h1 className="text-center pt-6 text-5xl text-blue-900/85 font-bold">2 Years+</h1>
+            <p className="text-center text-white/65 text-26px pb-6 pt-3">Average Client Lifecycle with VEX Media</p>
+            </div>
+            <div className="resize relative w-full h-38 px-2 my-auto rounded-2xl border border-white/25">
+            <h1 className="text-center pt-6 text-5xl text-blue-900/85 font-bold">28 Days</h1>
+            <p className="text-center text-white/65 text-26px pb-6 pt-3">Average Time After Client Gets Financially Positive ROI</p>
+            </div>
+        </div>
+    </div>)
+}
+
+   
