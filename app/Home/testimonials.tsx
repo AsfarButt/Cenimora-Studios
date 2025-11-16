68c9b1ec417d36be36abd3df33c8ab3fd20911de
AsfarButt <asfarbutt69@gmail.com>
2025-11-16 21:57:19 +0500

    Update 5

--- app/Home/testimonials.tsx
+++ app/Home/testimonials.tsx
@@ -13,33 +13,93 @@ const cormorant = Cormorant_Garamond({
 export default function Testimonials(){
     const [Testimonials, setTestimonials] = useState<number[]>([]);
     const activeindex = useRef(0);
+    const length = 15;
     const [activeproperties, setactiveproperties] = useState<string[]>([]);
-    const [translateposition, settranslateposition] = useState("translate-x-0");
 
     useEffect(() => {
-        const testimonials = Array(15).fill(0);
-        const properties = Array(testimonials.length).fill(["scale-80 z-0"]);
-        setactiveproperties(properties);
+        const testimonials = Array(length).fill(0);
         setTestimonials(testimonials);
-        activeindex.current = (testimonials.length-1)/2;
+        const actives3 = (activeindex.current-3+length)%length;
+        const actives2 = (activeindex.current-2+length)%length;
+        const actives1 = (activeindex.current-1+length)%length;
+        const activep1 = (activeindex.current+1)%length;
+        const activep2 = (activeindex.current+2)%length;
+        const activep3 = (activeindex.current+3)%length;
+        console.log("3 before Active Index: ",actives3);
+        console.log("2 before Active Index: ",actives2);
+        console.log("1 before Active Index: ",actives1);
+        console.log("Active Index: ",activeindex.current);
+        console.log("1 after Active Index: ",activep1);
+        console.log("2 after Active Index: ",activep2);
+        console.log("3 after Active Index: ",activep3);
+
+        ChangePlacement(actives3, actives2, actives1, activeindex.current, activep1, activep2, activep3);
     },[])
 
     function MoveLeft(){
-   
+        activeindex.current = (activeindex.current-1+length)%length;
+        const actives3 = (activeindex.current-3+length)%length;
+        const actives2 = (activeindex.current-2+length)%length;
+        const actives1 = (activeindex.current-1+length)%length;
+        const activep1 = (activeindex.current+1)%length;
+        const activep2 = (activeindex.current+2)%length;
+        const activep3 = (activeindex.current+3)%length;
+        console.log("3 before Active Index: ",actives3);
+        console.log("2 before Active Index: ",actives2);
+        console.log("1 before Active Index: ",actives1);
+        console.log("Active Index: ",activeindex.current);
+        console.log("1 after Active Index: ",activep1);
+        console.log("2 after Active Index: ",activep2);
+        console.log("3 after Active Index: ",activep3);
+
+        ChangePlacement(actives3, actives2, actives1, activeindex.current, activep1, activep2, activep3);
+    }
+
+    function MoveRight(){
+       activeindex.current = (activeindex.current+1)%length;
+        const actives3 = (activeindex.current-3+length)%length;
+        const actives2 = (activeindex.current-2+length)%length;
+        const actives1 = (activeindex.current-1+length)%length;
+        const activep1 = (activeindex.current+1)%length;
+        const activep2 = (activeindex.current+2)%length;
+        const activep3 = (activeindex.current+3)%length;
+        console.log("3 before Active Index: ",actives3);
+        console.log("2 before Active Index: ",actives2);
+        console.log("1 before Active Index: ",actives1);
+        console.log("Active Index: ",activeindex.current);
+        console.log("1 after Active Index: ",activep1);
+        console.log("2 after Active Index: ",activep2);
+        console.log("3 after Active Index: ",activep3);
+
+        ChangePlacement(actives3, actives2, actives1, activeindex.current, activep1, activep2, activep3);
+    }
+
+    function ChangePlacement(actives3: number, actives2: number, actives1: number, active: number, activep1: number, activep2: number, activep3: number){
+        const array1 = Array(length).fill("z-0 scale-10 translate-x-100");
+        array1[actives3] = "z-0 scale-0 -translate-x-100";
+        array1[actives2] = "z-1 scale-60 -translate-x-100";
+        array1[actives1] = "z-2 scale-80 -translate-x-70";
+        array1[active] = "z-3 scale-100 translate-x-0";
+        array1[activep1] = "z-2 scale-80 translate-x-70";
+        array1[activep2] = "z-1 scale-60 translate-x-100";
+        array1[activep3] = "z-0 scale-0 translate-x-100";
+
+        setactiveproperties(array1);
+
     }
 
     return(<div className="relative pt-20 w-full h-auto min-h-100 bg-black/98 flex flex-col justify-center">
                 <h1 className="text-white/90 text-4xl lg:text-5xl text-center">What people say <span className={`${cormorant.className} text-[46px] lg:text-[58px] text-blue-700/90`}>about us</span></h1>
 
-                <div className="relative mx-auto my-20 max-w-290 w-[90%] h-160 flex justify-center items-center bg-white overflow-hidden">
-                    <div className={`relative w-auto h-[70%] bg-pink-600/25 flex flex-row items-center`}>
+                <div className="relative mx-auto my-20 max-w-290 w-[90%] h-160 flex justify-center items-center bg-white">
+                    <div className={`relative w-75 h-[70%] flex flex-row items-center`}>
                         {Testimonials.map((element,index) => (
-                            <div className="flex-none h-full w-75 mx-4 rounded-2xl bg-white/85 border border-black" key={index}></div>
+                            <div className={`absolute flex-none h-full w-75 mr-6 transition-all duration-800 bg-white/85 shadow-sm shadow-black/40 text-8xl font-bold text-center ${activeproperties[index]}`} key={index}>{index}</div>
                         ))}
                     </div>
                     <div className="absolute px-2 w-full h-14 flex justify-between items-center z-1 transition-all duration-600">
-                        <button className="relative w-12 h-12 pr-3 text-black/90 text-2xl font-medium rounded-full bg-black/10">〈</button>
-                        <button className="relative w-12 h-12 pl-3 text-black/90 text-2xl font-medium rounded-full bg-black/10">〉</button>
+                        <button className="relative w-12 h-12 pr-3 text-black/90 text-2xl font-medium rounded-full bg-black/10" onClick={MoveLeft}>〈</button>
+                        <button className="relative w-12 h-12 pl-3 text-black/90 text-2xl font-medium rounded-full bg-black/10" onClick={MoveRight}>〉</button>
                     </div>
                 </div>
     </div>)
